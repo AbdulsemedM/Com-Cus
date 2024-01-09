212cb71c91d8aef52858f2b509cd28758f8d3d19
Abdulsemed Jemal <abdumussema22@gmail.com>
2024-01-09 14:15:06 +0300

    Adjust

---
+++ lib/features/forgot_password/forgot_password.dart
@@ -0,0 +1,170 @@
+import 'dart:convert';
+import 'package:commercepal/features/forgot_password/verify_otp.dart';
+import 'package:http/http.dart' as http;
+import 'package:flutter/material.dart';
+
+class ForgotPassword extends StatefulWidget {
+  const ForgotPassword({super.key});
+
+  @override
+  State<ForgotPassword> createState() => _ForgotPasswordState();
+}
+
+class _ForgotPasswordState extends State<ForgotPassword> {
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+
+  String? emailAddress;
+  var loading = false;
+  @override
+  Widget build(BuildContext context) {
+    var sHeight = MediaQuery.of(context).size.height * 1;
+    return Scaffold(
+      body: SafeArea(
+          child: SingleChildScrollView(
+        child: Column(
+          children: [
+            SizedBox(
+              height: sHeight * 0.25,
+              child:
+                  const Image(image: AssetImage("assets/images/app_icon.png")),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [Text("Forgot Password")],
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 16.0),
+              child: Form(
+                key: _formKey,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    const Padding(
+                      padding:
+                          EdgeInsets.symmetric(vertical: 8.0, horizontal: 4),
+                      child: Text(
+                        "Email",
+                        style: TextStyle(color: Colors.black),
+                      ),
+                    ),
+                    TextFormField(
+                      validator: (value) {
+                        if (value?.isEmpty == true) {
+                          return 'Email is required';
+                        }
+                        return null;
+                      },
+                      onChanged: (value) {
+                        setState(() {
+                          emailAddress = value;
+                        });
+                      },
+                      // title: "Password",
+                      decoration: InputDecoration(
+                        filled: true,
+                        fillColor: Colors.grey[100],
+                        enabledBorder: const OutlineInputBorder(
+                            borderSide: BorderSide.none),
+                        errorBorder: const OutlineInputBorder(
+                            borderSide: BorderSide.none),
+                        focusedBorder: const OutlineInputBorder(
+                            borderSide: BorderSide.none),
+                        focusedErrorBorder: const OutlineInputBorder(
+                            borderSide: BorderSide.none),
+                        hintText: "Enter your email or phone number",
+                      ),
+                      keyboardType: TextInputType.emailAddress,
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.all(16.0),
+                          child: loading
+                              ? CircularProgressIndicator()
+                              : ElevatedButton(
+                                  onPressed: () async {
+                                    if (_formKey.currentState!.validate()) {
+                                      bool done = await sendEmail();
+                                      if (done) {
+                                        // ignore: use_build_context_synchronously
+                                        Navigator.push(
+                                            context,
+                                            MaterialPageRoute(
+                                                builder: (context) => VerifyOTP(
+                                                      userName: emailAddress!,
+                                                    )));
+                                      }
+                                    }
+                                  },
+                                  child: Text("Submit")),
+                        ),
+                      ],
+                    )
+                  ],
+                ),
+              ),
+            )
+          ],
+        ),
+      )),
+    );
+  }
+
+  Future<bool> sendEmail({int retryCount = 0}) async {
+    try {
+      setState(() {
+        loading = true;
+      });
+      Map<String, dynamic> payload = {
+        "email": emailAddress.toString(),
+      };
+      print(payload);
+
+      final response = await http.post(
+        Uri.https(
+          "api.commercepal.com:2096",
+          "/prime/api/v1/password-reset",
+        ),
+        body: jsonEncode(payload),
+        // headers: <String, String>{"Authorization": "Bearer $token"},
+      );
+
+      var data = jsonDecode(response.body);
+      print(data);
+
+      if (data['statusCode'] == '000') {
+        setState(() {
+          loading = false;
+        });
+        return true;
+        // Handle the case when statusCode is '000'
+      } else {
+        // Retry logic
+        if (retryCount < 5) {
+          // Retry after num + 1 seconds
+          await Future.delayed(Duration(seconds: retryCount++));
+          // Call the function again with an increased retryCount
+          await sendEmail(retryCount: retryCount + 1);
+        } else {
+          // Retry limit reached, handle accordingly
+          setState(() {
+            loading = false;
+          });
+          return false;
+        }
+        return false;
+      }
+    } catch (e) {
+      print(e.toString());
+      setState(() {
+        loading = false;
+      });
+      // Handle other exceptions
+      return false;
+    }
+  }
+}
---
+++ lib/features/payment/presentation/payment_page.dart
@@ -0,0 +1,177 @@
+import 'dart:developer';
+
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:commercepal/app/app.dart';
+import 'package:commercepal/app/di/injector.dart';
+import 'package:commercepal/app/utils/app_colors.dart';
+import 'package:commercepal/core/customer_loan/data/dto/financial_mark_ups_dto.dart';
+import 'package:commercepal/features/cash_payment/presentation/cash_payment_page.dart';
+import 'package:commercepal/features/dashboard/widgets/home_error_widget.dart';
+import 'package:commercepal/features/dashboard/widgets/home_loading_widget.dart';
+import 'package:commercepal/features/payment/data/dto/payment_modes_dto.dart';
+import 'package:commercepal/features/payment/presentation/bloc/payment_cubit.dart';
+import 'package:commercepal/features/payment/presentation/bloc/payment_state.dart';
+import 'package:commercepal/features/sahay_payment/presentation/sahay_pay_page.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import '../../../app/utils/assets.dart';
+import '../../customer_loan/presentation/customer_loan_page.dart';
+import 'loan_payment_period_bs.dart';
+
+class PaymentPage extends StatelessWidget {
+  static const routeName = "/payment_page";
+
+  const PaymentPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        title: Text(
+          'Payment modes',
+          style: Theme.of(context).textTheme.titleMedium,
+        ),
+        centerTitle: false,
+      ),
+      body: BlocProvider(
+        create: (context) => getIt<PaymentCubit>()..fetchPaymentModes(),
+        child: BlocConsumer<PaymentCubit, PaymentState>(
+          listener: (context, state) {},
+          builder: (ctx, state) {
+            return state.maybeWhen(
+                orElse: () => const HomeLoadingWidget(),
+                error: (error) => HomeErrorWidget(error: error),
+                paymentMethods: (paymentModes) => Padding(
+                      padding: const EdgeInsets.all(16.0),
+                      child: ListView(
+                        children: paymentModes
+                            .map((e) => Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    Text(
+                                      e.name ?? '',
+                                      style: Theme.of(context)
+                                          .textTheme
+                                          .titleMedium
+                                          ?.copyWith(color: Colors.black),
+                                    ),
+                                    const SizedBox(
+                                      height: 18,
+                                    ),
+                                    Container(
+                                      height: e.items!.length > 3
+                                          ? MediaQuery.of(context).size.height *
+                                              0.25
+                                          : MediaQuery.of(context).size.height *
+                                              0.12,
+                                      child: GridView.builder(
+                                        gridDelegate:
+                                            SliverGridDelegateWithFixedCrossAxisCount(
+                                          crossAxisCount:
+                                              3, // Number of items in each row
+                                          crossAxisSpacing:
+                                              4.0, // Spacing between columns
+                                          mainAxisSpacing:
+                                              0, // Spacing between rows
+                                        ),
+                                        itemCount: e.items!.length,
+                                        itemBuilder:
+                                            (BuildContext context, int index) {
+                                          var item = e.items![index];
+
+                                          return InkWell(
+                                            onTap: () {
+                                              _redirectUserBasedOnPaymentMethod(
+                                                  item, context);
+                                            },
+                                            child: Padding(
+                                              padding:
+                                                  const EdgeInsets.all(0),
+                                              child: Column(
+                                                children: [
+                                                  Container(
+                                                    padding:
+                                                        const EdgeInsets.all(
+                                                            12),
+                                                    height: 60,
+                                                    width: 60,
+                                                    decoration:
+                                                        const BoxDecoration(
+                                                      shape: BoxShape.circle,
+                                                      color: AppColors.bg1,
+                                                    ),
+                                                    child: item.iconUrl ==
+                                                            "loan.png"
+                                                        ? Image.asset(
+                                                            Assets.bank)
+                                                        : CachedNetworkImage(
+                                                            imageUrl:
+                                                                item.iconUrl ??
+                                                                    '',
+                                                          ),
+                                                  ),
+                                                  const SizedBox(
+                                                    height: 5,
+                                                  ),
+                                                  Text(item.name ?? ''),
+                                                ],
+                                              ),
+                                            ),
+                                          );
+                                        },
+                                      ),
+                                    ),
+                                    const SizedBox(
+                                      height: 10,
+                                    ),
+                                    const Divider(),
+                                    const SizedBox(
+                                      height: 10,
+                                    ),
+                                  ],
+                                ))
+                            .toList(),
+                      ),
+                    ));
+          },
+        ),
+      ),
+    );
+  }
+
+  void _redirectUserBasedOnPaymentMethod(
+      PaymentMethodItem e, BuildContext context) {
+    if (e.name!.toLowerCase().contains("sahay") == true) {
+      Navigator.pushNamed(context, SahayPayPage.routeName, arguments: {
+        "cash_type": e.name,
+        "payment_instruction": e.paymentInstruction
+      });
+    } else if (e.paymentMode == PaymentMode.loan) {
+      _showModalBottomSheet(context, e.id!, (MarkUpItem markUpItem) {
+        Navigator.pushNamed(context, CustomerLoanPage.routeName, arguments: {
+          "name": "Loan payment via ${e.name}",
+          "institutionId": e.id,
+          "markUp": markUpItem
+        });
+      });
+    } else {
+      Navigator.pushNamed(context, CashPaymentPage.routeName, arguments: {
+        "cash_type": e.paymentType,
+        "cash_type_name": e.name,
+        "payment_instruction": e.paymentInstruction
+      });
+    }
+  }
+}
+
+_showModalBottomSheet(
+    BuildContext context, num institutionId, Function onPeriodClicked) {
+  showModalBottomSheet(
+      context: context,
+      builder: (ctx) => LoanPaymentPeriodBs(
+            institutionId: institutionId,
+            onPeriodClicked: onPeriodClicked,
+          ));
+}
