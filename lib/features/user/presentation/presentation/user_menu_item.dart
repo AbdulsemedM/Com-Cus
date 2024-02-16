f30ea71e3bc8f0e6a270605fa5c91b36ce2ae11b
Abdulsemed Jemal <abdumussema22@gmail.com>
2024-02-16 11:14:49 +0300

    Adjust

--- lib/features/user/presentation/presentation/user_menu_item.dart
+++ lib/features/user/presentation/presentation/user_menu_item.dart
@@ -46,32 +46,6 @@ class UserMenuItem extends StatelessWidget {
                   ?.copyWith(color: AppColors.colorPrimary, fontSize: 14.sp),
               textAlign: TextAlign.right,
             ),
-            // FutureBuilder<String>(
-            //   future: Translations.translatedText(
-            //       title, GlobalStrings.getGlobalString()),
-            //   builder: (context, snapshot) {
-            //     if (snapshot.connectionState == ConnectionState.done) {
-            //       return Text(
-            //         snapshot.data ?? 'Default Text',
-            //         style: Theme.of(context).textTheme.titleMedium?.copyWith(
-            //             color: AppColors.colorPrimary, fontSize: 14.sp),
-            //         textAlign: TextAlign.right,
-            //       );
-            //     } else {
-            //       return Text(
-            //         'Loading...',
-            //         textAlign: TextAlign.right,
-            //       ); // Or any loading indicator
-            //     }
-            //   },
-            // ),
-            // Text(
-            //   title,
-            //   style: Theme.of(context)
-            //       .textTheme
-            //       .titleMedium
-            //       ?.copyWith(fontSize: 14.sp),
-            // ),
             const Spacer(),
             const Icon(
               Icons.arrow_forward_ios_outlined,
